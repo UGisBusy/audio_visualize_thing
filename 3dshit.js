27f11a4fc8a48298c8b1d5b235dea1f7b3ae4c8f
UG <ug00257793@gmail.com>
2024-01-05 04:11:14 +0800

    final?

--- 3dshit.js
+++ 3dshit.js
@@ -17,7 +17,7 @@ function keyPressed() {
 
 var FFT_CNT = 1024;
 var TOTAL_LENGTH = 500;
-var FULL_SIDE_CNT = 31;
+var FULL_SIDE_CNT = 41;
 var FFT_RATIO;
 var HALF_SIDE_CNT;
 var HEIGHT_FACTOR = 2;
